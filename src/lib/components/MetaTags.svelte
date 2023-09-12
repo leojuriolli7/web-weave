b4b0a33f4c233f44808741efde265492f7054646
leojuriolli7 <leojuriolli@gmail.com>
2023-09-11 23:32:50 -0300

    fix: resolvedImage

--- src/lib/components/MetaTags.svelte
+++ src/lib/components/MetaTags.svelte
@@ -4,6 +4,8 @@
 	export let title: string;
 	export let description: string;
 	export let image: string | null | undefined = undefined;
+
+	$: resolvedImage = image || 'https://web-weave.vercel.app/og.png';
 </script>
 
 <svelte:head>
@@ -14,12 +16,12 @@
 	<meta property="og:url" content={$page.url.href} />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content={image || `https://web-weave.vercel.app/og.png`} />
+	<meta property="og:image" content={resolvedImage} />
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content={$page.url.href} />
 	<meta property="twitter:title" content={title} />
 	<meta property="twitter:description" content={description} />
-	<meta property="twitter:image" content={image || 'https://web-weave.vercel.app/og.png'} />
+	<meta property="twitter:image" content={resolvedImage} />
 </svelte:head>
