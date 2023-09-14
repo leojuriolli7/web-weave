cde32b71210f698bfc784ac5138ab13553be46e2
leojuriolli7 <leojuriolli@gmail.com>
2023-09-13 23:20:00 -0300

    test: preload font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,15 +28,6 @@
 	});
 </script>
 
-<svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-		rel="stylesheet"
-	/>
-</svelte:head>
-
 <slot />
 
 <style>
